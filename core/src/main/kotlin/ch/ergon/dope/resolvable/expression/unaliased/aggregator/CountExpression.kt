495d1fd76a62534e335800d799b2042e8b391fe4
Jan Sigrist <jan.sigrist@ergon.ch>
2024-03-21 10:09:28 +0100

    DOPE-193: added support for count with tests

---
+++ core/src/main/kotlin/ch/ergon/dope/resolvable/expression/unaliased/aggregator/CountExpression.kt
@@ -0,0 +1,16 @@
+package ch.ergon.dope.resolvable.expression.unaliased.aggregator
+
+import ch.ergon.dope.resolvable.expression.unaliased.type.Field
+import ch.ergon.dope.resolvable.formatToQueryStringWithBrackets
+import ch.ergon.dope.validtype.StringType
+import ch.ergon.dope.validtype.ValidType
+
+class CountExpression(
+    private val field: Field<out ValidType>
+): AggregateExpression {
+    override fun toQueryString(): String = formatToQueryStringWithBrackets("COUNT", field)
+}
+
+fun count(field: Field<out ValidType>) = CountExpression(field)
+
+fun countAll() = CountExpression(Field<StringType>("*", ""))
--- core/src/test/kotlin/ch/ergon/dope/NumberFunctionsTest.kt
+++ core/src/test/kotlin/ch/ergon/dope/NumberFunctionsTest.kt
@@ -1,6 +1,8 @@
 package ch.ergon.dope
 
 import ch.ergon.dope.resolvable.expression.alias
+import ch.ergon.dope.resolvable.expression.unaliased.aggregator.count
+import ch.ergon.dope.resolvable.expression.unaliased.aggregator.countAll
 import ch.ergon.dope.resolvable.expression.unaliased.aggregator.min
 import ch.ergon.dope.resolvable.expression.unaliased.type.toNumberType
 import junit.framework.TestCase.assertEquals
@@ -39,6 +41,34 @@ class NumberFunctionsTest {
                 min(TestBucket.Person.fname),
             ).from(
                 TestBucket.Person,
+            ).build()
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `should support count all`() {
+        val expected = "SELECT COUNT(*) FROM person"
+
+        val actual: String = create
+            .select(
+                countAll(),
+            ).from(
+                TestBucket.Person,
+            ).build()
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `should support count with a Field`() {
+        val expected = "SELECT COUNT(route.id) FROM route"
+
+        val actual: String = create
+            .select(
+                count(TestBucket.Route.id),
+            ).from(
+                TestBucket.Route,
             ).build()
 
         assertEquals(expected, actual)
