33b6d6d274900745c88c4b4bc6bcee72783f200a
Jan Sigrist <jan.sigrist@ergon.ch>
2024-04-03 12:51:52 +0200

    DOPE-198: renamed ValuedParameter -> Parameter

--- core/src/main/kotlin/ch/ergon/dope/resolvable/expression/unaliased/type/Parameter.kt
+++ core/src/main/kotlin/ch/ergon/dope/resolvable/expression/unaliased/type/Parameter.kt
@@ -3,20 +3,20 @@ package ch.ergon.dope.resolvable.expression.unaliased.type
 import ch.ergon.dope.resolvable.expression.TypeExpression
 import ch.ergon.dope.validtype.ValidType
 
-class ValuedParameter<T : ValidType>(val value: TypeExpression<T>, private val parameterName: String?) : TypeExpression<T> {
+class Parameter<T : ValidType>(val value: TypeExpression<T>, private val parameterName: String?) : TypeExpression<T> {
     override fun toQueryString(): String = when (parameterName) {
         null -> "\$${ParameterCounter.count}"
         else -> "\$$parameterName"
     }
 }
 
-fun <T : ValidType> TypeExpression<T>.asParameter(parameterName: String) = ValuedParameter(this, parameterName)
+fun <T : ValidType> TypeExpression<T>.asParameter(parameterName: String) = Parameter(this, parameterName)
 
-fun String.asParameter(parameterName: String) = ValuedParameter(this.toStringType(), parameterName)
+fun String.asParameter(parameterName: String) = Parameter(this.toStringType(), parameterName)
 
-fun <T : ValidType> TypeExpression<T>.asParameter() = ValuedParameter(this, null)
+fun <T : ValidType> TypeExpression<T>.asParameter() = Parameter(this, null)
 
-fun String.asParameter() = ValuedParameter(this.toStringType(), null)
+fun String.asParameter() = Parameter(this.toStringType(), null)
 
 private object ParameterCounter {
     var count: Int = 1
