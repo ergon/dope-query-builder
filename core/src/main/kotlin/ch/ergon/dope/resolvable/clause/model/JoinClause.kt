e8878aeff2524c75d9ec4b804e3a70a5a73c4049
Martina Gallati <martina.gallati@ergon.ch>
2024-06-24 10:16:26 +0200

    DOPE-179 added JoinType enum, refactoring

--- core/src/main/kotlin/ch/ergon/dope/resolvable/clause/model/JoinClause.kt
+++ core/src/main/kotlin/ch/ergon/dope/resolvable/clause/model/JoinClause.kt
@@ -3,48 +3,57 @@ package ch.ergon.dope.resolvable.clause.model
 import ch.ergon.dope.DopeQuery
 import ch.ergon.dope.resolvable.clause.ISelectFromClause
 import ch.ergon.dope.resolvable.clause.ISelectJoinClause
+import ch.ergon.dope.resolvable.clause.model.JoinType.INNER_JOIN
+import ch.ergon.dope.resolvable.clause.model.JoinType.JOIN
+import ch.ergon.dope.resolvable.clause.model.JoinType.LEFT_JOIN
+import ch.ergon.dope.resolvable.clause.model.JoinType.RIGHT_JOIN
 import ch.ergon.dope.resolvable.expression.TypeExpression
 import ch.ergon.dope.resolvable.expression.unaliased.type.Field
 import ch.ergon.dope.resolvable.fromable.Bucket
 import ch.ergon.dope.validtype.BooleanType
 import ch.ergon.dope.validtype.ValidType
 
-private const val JOIN = "JOIN"
-private const val LEFT_JOIN = "LEFT JOIN"
-private const val INNER_JOIN = "INNER JOIN"
+private enum class JoinType(val type: String) {
+    JOIN("JOIN"),
+    LEFT_JOIN("LEFT JOIN"),
+    INNER_JOIN("INNER JOIN"),
+    RIGHT_JOIN("RIGHT JOIN"),
+}
 
 sealed class SelectJoinClause : ISelectJoinClause {
     private val dopeQuery: DopeQuery
 
-    constructor(joinType: String, bucket: Bucket, onCondition: TypeExpression<BooleanType>, parentClause: ISelectFromClause) {
+    constructor(joinType: JoinType, bucket: Bucket, onCondition: TypeExpression<BooleanType>, parentClause: ISelectFromClause) {
         val parentDopeQuery = parentClause.toDopeQuery()
         val bucketDopeQuery = bucket.toDopeQuery()
         val onConditionDopeQuery = onCondition.toDopeQuery()
         dopeQuery = DopeQuery(
-            queryString = "${parentDopeQuery.queryString} $joinType ${bucketDopeQuery.queryString} ON ${onConditionDopeQuery.queryString}",
+            queryString = "${parentDopeQuery.queryString} ${joinType.type} ${bucketDopeQuery.queryString} " +
+                "ON ${onConditionDopeQuery.queryString}",
             parameters = parentDopeQuery.parameters + bucketDopeQuery.parameters + onConditionDopeQuery.parameters,
         )
     }
 
-    constructor(joinType: String, bucket: Bucket, key: Field<out ValidType>, parentClause: ISelectFromClause) {
+    constructor(joinType: JoinType, bucket: Bucket, onKeys: Field<out ValidType>, parentClause: ISelectFromClause) {
         val parentDopeQuery = parentClause.toDopeQuery()
         val bucketDopeQuery = bucket.toDopeQuery()
-        val keyDopeQuery = key.toDopeQuery()
+        val keyDopeQuery = onKeys.toDopeQuery()
         dopeQuery = DopeQuery(
-            queryString = "${parentDopeQuery.queryString} $joinType ${bucketDopeQuery.queryString} ON KEYS ${keyDopeQuery.queryString}",
+            queryString = "${parentDopeQuery.queryString} ${joinType.type} ${bucketDopeQuery.queryString} " +
+                "ON KEYS ${keyDopeQuery.queryString}",
             parameters = parentDopeQuery.parameters + bucketDopeQuery.parameters + keyDopeQuery.parameters,
         )
     }
 
-    constructor(joinType: String, bucket: Bucket, key: Field<out ValidType>, forBucket: Bucket, parentClause: ISelectFromClause) {
+    constructor(joinType: JoinType, bucket: Bucket, onKey: Field<out ValidType>, forBucket: Bucket, parentClause: ISelectFromClause) {
         val parentDopeQuery = parentClause.toDopeQuery()
         val bucketDopeQuery = bucket.toDopeQuery()
-        val keyDopeQuery = key.toDopeQuery()
+        val keyDopeQuery = onKey.toDopeQuery()
         val forBucketDopeQuery = forBucket.toDopeQuery()
         dopeQuery = DopeQuery(
-            queryString = "${parentDopeQuery.queryString} $joinType ${bucketDopeQuery.queryString} " +
+            queryString = "${parentDopeQuery.queryString} ${joinType.type} ${bucketDopeQuery.queryString} " +
                 "ON KEY ${keyDopeQuery.queryString} FOR ${forBucketDopeQuery.queryString}",
-            parameters = parentDopeQuery.parameters + bucketDopeQuery.parameters + keyDopeQuery.parameters,
+            parameters = parentDopeQuery.parameters + bucketDopeQuery.parameters + keyDopeQuery.parameters + forBucketDopeQuery.parameters,
         )
     }
 
@@ -85,4 +94,4 @@ class InnerJoinClause : SelectJoinClause {
 }
 
 class RightJoinClause(bucket: Bucket, onCondition: TypeExpression<BooleanType>, parentClause: ISelectFromClause) :
-    SelectJoinClause("RIGHT JOIN", bucket, onCondition, parentClause)
+    SelectJoinClause(RIGHT_JOIN, bucket, onCondition, parentClause)
