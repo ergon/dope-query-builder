83fdc306158d54e761f7fffaa237522ef88a8133
Jan Sigrist <jan.sigrist@ergon.ch>
2024-08-05 10:59:59 +0200

    DOPE-243: extracted toDopeQuery for Primitive

--- core/src/main/kotlin/ch/ergon/dope/resolvable/expression/unaliased/type/Primitive.kt
+++ core/src/main/kotlin/ch/ergon/dope/resolvable/expression/unaliased/type/Primitive.kt
@@ -11,46 +11,54 @@ import ch.ergon.dope.validtype.StringType
 import ch.ergon.dope.validtype.ValidType
 
 sealed class Primitive<T : ValidType>(
-    private val dopeQuery: DopeQuery,
+    private val dopeQuery: () -> DopeQuery,
 ) : TypeExpression<T> {
-    override fun toDopeQuery() = dopeQuery
+    override fun toDopeQuery() = dopeQuery()
 }
 
-data object NULL : Primitive<NullType>(DopeQuery("NULL", emptyMap()))
-data object MISSING : Primitive<MissingType>(DopeQuery("MISSING", emptyMap()))
-data object TRUE : Primitive<BooleanType>(DopeQuery("TRUE", emptyMap()))
-data object FALSE : Primitive<BooleanType>(DopeQuery("FALSE", emptyMap()))
+data object NULL : Primitive<NullType>({ DopeQuery("NULL", emptyMap()) })
+data object MISSING : Primitive<MissingType>({ DopeQuery("MISSING", emptyMap()) })
+data object TRUE : Primitive<BooleanType>({ DopeQuery("TRUE", emptyMap()) })
+data object FALSE : Primitive<BooleanType>({ DopeQuery("FALSE", emptyMap()) })
 
 class NumberPrimitive(value: Number) : Primitive<NumberType>(
-    DopeQuery(
-        queryString = "$value",
-        parameters = emptyMap(),
-    ),
+    {
+        DopeQuery(
+            queryString = "$value",
+            parameters = emptyMap(),
+        )
+    },
 )
 
 class StringPrimitive(value: String) : Primitive<StringType>(
-    DopeQuery(
-        queryString = "\"$value\"",
-        parameters = emptyMap(),
-    ),
+    {
+        DopeQuery(
+            queryString = "\"$value\"",
+            parameters = emptyMap(),
+        )
+    },
 )
 
 class BooleanPrimitive(value: Boolean) : Primitive<BooleanType>(
-    DopeQuery(
-        queryString = when (value) {
-            true -> TRUE.toDopeQuery().queryString
-            false -> FALSE.toDopeQuery().queryString
-        },
-        parameters = emptyMap(),
-    ),
+    {
+        DopeQuery(
+            queryString = when (value) {
+                true -> TRUE.toDopeQuery().queryString
+                false -> FALSE.toDopeQuery().queryString
+            },
+            parameters = emptyMap(),
+        )
+    },
 )
 
 class ArrayPrimitive<T : ValidType>(collection: Collection<TypeExpression<out T>>) : Primitive<ArrayType<T>>(
-    collection.map { it.toDopeQuery() }.let { dopeQueries ->
-        DopeQuery(
-            queryString = dopeQueries.joinToString(", ", prefix = "[", postfix = "]") { it.queryString },
-            parameters = dopeQueries.fold(emptyMap()) { parameters, dopeQueryElement -> parameters + dopeQueryElement.parameters },
-        )
+    {
+        collection.map { it.toDopeQuery() }.let { dopeQueries ->
+            DopeQuery(
+                queryString = dopeQueries.joinToString(", ", prefix = "[", postfix = "]") { it.queryString },
+                parameters = dopeQueries.fold(emptyMap()) { parameters, dopeQueryElement -> parameters + dopeQueryElement.parameters },
+            )
+        }
     },
 )
 
