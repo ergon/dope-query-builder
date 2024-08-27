c616e207f20b149ff3b96c40300f24fec9b657d6
Jan Sigrist <jan.sigrist@ergon.ch>
2024-08-27 09:57:38 +0200

    DOPE-218: use expression body

--- core/src/main/kotlin/ch/ergon/dope/resolvable/clause/Clause.kt
+++ core/src/main/kotlin/ch/ergon/dope/resolvable/clause/Clause.kt
@@ -1,11 +1,8 @@
 package ch.ergon.dope.resolvable.clause
 
-import ch.ergon.dope.DopeQuery
 import ch.ergon.dope.DopeQueryManager
 import ch.ergon.dope.resolvable.Resolvable
 
 interface Clause : Resolvable {
-    fun build(): DopeQuery {
-        return toDopeQuery(manager = DopeQueryManager())
-    }
+    fun build() = toDopeQuery(manager = DopeQueryManager())
 }
