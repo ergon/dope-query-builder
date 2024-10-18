4a6aa99232e8fe7b33bf246e6ca186ef87d5d013
Martina Gallati <martina.gallati@ergon.ch>
2024-10-18 17:41:20 +0200

    DOPE-258: moved DopeParameters into their own file, added KDoc

--- core/src/main/kotlin/ch/ergon/dope/DopeQuery.kt
+++ core/src/main/kotlin/ch/ergon/dope/DopeQuery.kt
@@ -4,26 +4,3 @@ data class DopeQuery(
     val queryString: String,
     val parameters: DopeParameters = DopeParameters(),
 )
-
-data class DopeParameters(
-    val namedParameters: Map<String, Any> = emptyMap(),
-    val positionalParameters: List<Any> = emptyList(),
-) {
-    fun merge(vararg otherParameters: DopeParameters?) = this.copy(
-        namedParameters = otherParameters.filterNotNull().fold(this.namedParameters) {
-                namedParams, additionalNamedParams ->
-            namedParams + additionalNamedParams.namedParameters
-        },
-        positionalParameters = otherParameters.filterNotNull().fold(this.positionalParameters) {
-                positionalParams, additionalPositionalParams ->
-            positionalParams + additionalPositionalParams.positionalParameters
-        },
-    )
-}
-
-fun List<DopeParameters>.merge(vararg otherParameters: DopeParameters): DopeParameters {
-    return this.fold(DopeParameters()) {
-            parameters, additionalParameters ->
-        parameters.merge(additionalParameters)
-    }.merge(*otherParameters)
-}
