ce587e9607a9277185b7e5f8ccfa5def37f25809
Jan Sigrist <jan.sigrist@ergon.ch>
2024-10-22 11:39:18 +0200

    DOPE-261: change like to support usage with TypeExpression

--- crystal-map-connector/src/main/kotlin/ch/ergon/dope/extension/type/relational/Like.kt
+++ crystal-map-connector/src/main/kotlin/ch/ergon/dope/extension/type/relational/Like.kt
@@ -1,39 +1,25 @@
 package ch.ergon.dope.extension.type.relational
 
 import ch.ergon.dope.resolvable.expression.TypeExpression
-import ch.ergon.dope.resolvable.expression.unaliased.type.Field
 import ch.ergon.dope.resolvable.expression.unaliased.type.relational.isLike
 import ch.ergon.dope.resolvable.expression.unaliased.type.relational.isNotLike
 import ch.ergon.dope.resolvable.expression.unaliased.type.toDopeType
 import ch.ergon.dope.toDopeType
 import ch.ergon.dope.validtype.StringType
-import ch.ergon.dope.validtype.ValidType
 import com.schwarz.crystalapi.schema.CMJsonField
 
-fun Field<out ValidType>.isLike(right: CMJsonField<String>) = isLike(right.toDopeType())
+fun TypeExpression<StringType>.isLike(right: CMJsonField<String>) = isLike(right.toDopeType())
 
-@JvmName("isLikeNumber")
-fun CMJsonField<out Number>.isLike(right: String) = toDopeType().isLike(right.toDopeType())
+fun String.isLike(right: CMJsonField<String>) = toDopeType().isLike(right.toDopeType())
 
-@JvmName("isLikeString")
 fun CMJsonField<String>.isLike(right: String) = toDopeType().isLike(right.toDopeType())
 
-@JvmName("isLikeString")
 fun CMJsonField<String>.isLike(right: TypeExpression<StringType>) = toDopeType().isLike(right)
 
-@JvmName("isLikeBoolean")
-fun CMJsonField<Boolean>.isLike(right: String) = toDopeType().isLike(right.toDopeType())
+fun TypeExpression<StringType>.isNotLike(right: CMJsonField<String>) = isNotLike(right.toDopeType())
 
-fun Field<out ValidType>.isNotLike(right: CMJsonField<String>) = isNotLike(right.toDopeType())
+fun String.isNotLike(right: CMJsonField<String>) = toDopeType().isNotLike(right.toDopeType())
 
-@JvmName("isNotLikeNumber")
-fun CMJsonField<out Number>.isNotLike(right: String) = toDopeType().isNotLike(right.toDopeType())
-
-@JvmName("isNotLikeString")
 fun CMJsonField<String>.isNotLike(right: String) = toDopeType().isNotLike(right.toDopeType())
 
-@JvmName("isNotLikeString")
 fun CMJsonField<String>.isNotLike(right: TypeExpression<StringType>) = toDopeType().isNotLike(right)
-
-@JvmName("isNotLikeBoolean")
-fun CMJsonField<Boolean>.isNotLike(right: String) = toDopeType().isNotLike(right.toDopeType())
--- crystal-map-connector/src/test/kotlin/ch/ergon/dope/extensions/type/relational/LikeTest.kt
+++ crystal-map-connector/src/test/kotlin/ch/ergon/dope/extensions/type/relational/LikeTest.kt
@@ -5,6 +5,8 @@ import ch.ergon.dope.extension.type.relational.isLike
 import ch.ergon.dope.extension.type.relational.isNotLike
 import ch.ergon.dope.helper.ManagerDependentTest
 import ch.ergon.dope.helper.someCMStringField
+import ch.ergon.dope.helper.someString
+import ch.ergon.dope.helper.someStringField
 import ch.ergon.dope.resolvable.expression.unaliased.type.relational.LikeExpression
 import ch.ergon.dope.resolvable.expression.unaliased.type.relational.NotLikeExpression
 import ch.ergon.dope.resolvable.expression.unaliased.type.toDopeType
@@ -16,20 +18,86 @@ class LikeTest : ManagerDependentTest {
     override lateinit var manager: DopeQueryManager
 
     @Test
-    fun `should support extensions for is like with CMJsonField`() {
-        val someCMStringField = someCMStringField()
-        val right = "%".toDopeType()
-        val expected = LikeExpression(someCMStringField.toDopeType(), right)
+    fun `should support extensions for is like with type cmString`() {
+        val left = someStringField()
+        val right = someCMStringField()
+        val expected = LikeExpression(left, right.toDopeType())
 
-        val actual = someCMStringField.isLike(right)
+        val actual = left.isLike(right)
 
         assertEquals(expected.toDopeQuery(manager), actual.toDopeQuery(manager))
     }
 
     @Test
-    fun `should support extensions for is not like with CMJsonField`() {
+    fun `should support extensions for is like with string cmString`() {
+        val left = someString()
+        val right = someCMStringField()
+        val expected = LikeExpression(left.toDopeType(), right.toDopeType())
+
+        val actual = left.isLike(right)
+
+        assertEquals(expected.toDopeQuery(manager), actual.toDopeQuery(manager))
+    }
+
+    @Test
+    fun `should support extensions for is like with cmString`() {
+        val left = someCMStringField()
+        val right = someString()
+        val expected = LikeExpression(left.toDopeType(), right.toDopeType())
+
+        val actual = left.isLike(right)
+
+        assertEquals(expected.toDopeQuery(manager), actual.toDopeQuery(manager))
+    }
+
+    @Test
+    fun `should support extensions for is like with cm`() {
+        val left = someCMStringField()
+        val right = someStringField()
+        val expected = LikeExpression(left.toDopeType(), right)
+
+        val actual = left.isLike(right)
+
+        assertEquals(expected.toDopeQuery(manager), actual.toDopeQuery(manager))
+    }
+
+    @Test
+    fun `should support extensions for is not like with type cmString`() {
+        val left = someStringField()
+        val right = someCMStringField()
+        val expected = NotLikeExpression(left, right.toDopeType())
+
+        val actual = left.isNotLike(right)
+
+        assertEquals(expected.toDopeQuery(manager), actual.toDopeQuery(manager))
+    }
+
+    @Test
+    fun `should support extensions for is not like with string cmString`() {
+        val left = someString()
+        val right = someCMStringField()
+        val expected = NotLikeExpression(left.toDopeType(), right.toDopeType())
+
+        val actual = left.isNotLike(right)
+
+        assertEquals(expected.toDopeQuery(manager), actual.toDopeQuery(manager))
+    }
+
+    @Test
+    fun `should support extensions for is not like with cmString`() {
+        val left = someCMStringField()
+        val right = someString()
+        val expected = NotLikeExpression(left.toDopeType(), right.toDopeType())
+
+        val actual = left.isNotLike(right)
+
+        assertEquals(expected.toDopeQuery(manager), actual.toDopeQuery(manager))
+    }
+
+    @Test
+    fun `should support extensions for is not like with cm`() {
         val someCMStringField = someCMStringField()
-        val right = "%".toDopeType()
+        val right = someStringField()
         val expected = NotLikeExpression(someCMStringField.toDopeType(), right)
 
         val actual = someCMStringField.isNotLike(right)
