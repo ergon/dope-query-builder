c2831050ad0d385ba859db83c19e2452de1ecf9c
Jan Sigrist <jan.sigrist@ergon.ch>
2024-10-30 15:12:28 +0100

    DOPE-264: correct the type of some String Functions

--- core/src/main/kotlin/ch/ergon/dope/resolvable/expression/unaliased/type/function/stringfunction/SuffixesExpression.kt
+++ core/src/main/kotlin/ch/ergon/dope/resolvable/expression/unaliased/type/function/stringfunction/SuffixesExpression.kt
@@ -3,10 +3,11 @@ package ch.ergon.dope.resolvable.expression.unaliased.type.function.stringfuncti
 import ch.ergon.dope.resolvable.expression.TypeExpression
 import ch.ergon.dope.resolvable.expression.unaliased.type.function.FunctionExpression
 import ch.ergon.dope.resolvable.expression.unaliased.type.toDopeType
+import ch.ergon.dope.validtype.ArrayType
 import ch.ergon.dope.validtype.StringType
 
 class SuffixesExpression(inStr: TypeExpression<StringType>) :
-    FunctionExpression<StringType>("SUFFIXES", inStr)
+    FunctionExpression<ArrayType<StringType>>("SUFFIXES", inStr)
 
 fun suffixes(inStr: TypeExpression<StringType>) = SuffixesExpression(inStr)
 
