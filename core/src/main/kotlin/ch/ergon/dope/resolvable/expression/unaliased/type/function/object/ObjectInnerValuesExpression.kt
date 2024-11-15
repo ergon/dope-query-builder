ffe1220bb73018974fc538f69f51d944b6531898
Jan Sigrist <jan.sigrist@ergon.ch>
2024-11-15 09:42:01 +0100

    DOPE-239: correct type

--- core/src/main/kotlin/ch/ergon/dope/resolvable/expression/unaliased/type/function/object/ObjectInnerValuesExpression.kt
+++ core/src/main/kotlin/ch/ergon/dope/resolvable/expression/unaliased/type/function/object/ObjectInnerValuesExpression.kt
@@ -4,9 +4,10 @@ import ch.ergon.dope.resolvable.expression.TypeExpression
 import ch.ergon.dope.resolvable.expression.unaliased.type.function.FunctionExpression
 import ch.ergon.dope.validtype.ArrayType
 import ch.ergon.dope.validtype.ObjectType
+import ch.ergon.dope.validtype.StringType
 
 class ObjectInnerValuesExpression(
     objectExpression: TypeExpression<ObjectType>,
-) : FunctionExpression<ArrayType<ObjectType>>("OBJECT_INNER_VALUES", objectExpression)
+) : FunctionExpression<ArrayType<StringType>>("OBJECT_INNER_VALUES", objectExpression)
 
 fun TypeExpression<ObjectType>.innerValues() = ObjectInnerValuesExpression(this)
