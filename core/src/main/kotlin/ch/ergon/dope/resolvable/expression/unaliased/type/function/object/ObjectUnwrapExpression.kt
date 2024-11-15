b110cd09c04bc6de6c500348b01c80fa02374a1a
Jan Sigrist <jan.sigrist@ergon.ch>
2024-11-15 09:42:01 +0100

    DOPE-239: correct type

--- core/src/main/kotlin/ch/ergon/dope/resolvable/expression/unaliased/type/function/object/ObjectUnwrapExpression.kt
+++ core/src/main/kotlin/ch/ergon/dope/resolvable/expression/unaliased/type/function/object/ObjectUnwrapExpression.kt
@@ -3,9 +3,10 @@ package ch.ergon.dope.resolvable.expression.unaliased.type.function.`object`
 import ch.ergon.dope.resolvable.expression.TypeExpression
 import ch.ergon.dope.resolvable.expression.unaliased.type.function.FunctionExpression
 import ch.ergon.dope.validtype.ObjectType
+import ch.ergon.dope.validtype.ValidType
 
 class ObjectUnwrapExpression(
     objectExpression: TypeExpression<ObjectType>,
-) : FunctionExpression<ObjectType>("OBJECT_UNWRAP", objectExpression)
+) : FunctionExpression<ValidType>("OBJECT_UNWRAP", objectExpression)
 
 fun TypeExpression<ObjectType>.unwrap() = ObjectUnwrapExpression(this)
