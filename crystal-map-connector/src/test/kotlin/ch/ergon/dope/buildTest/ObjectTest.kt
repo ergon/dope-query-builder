d45af5b575d82238c1d5f7cb7ec1a91ac5f9d04d
Jan Sigrist <jan.sigrist@ergon.ch>
2024-11-19 08:52:36 +0100

    DOPE-216: added build test for ObjectField

--- crystal-map-connector/src/test/kotlin/ch/ergon/dope/buildTest/ObjectTest.kt
+++ crystal-map-connector/src/test/kotlin/ch/ergon/dope/buildTest/ObjectTest.kt
@@ -9,6 +9,11 @@ import ch.ergon.dope.extension.type.relational.isEqualTo
 import ch.ergon.dope.extension.type.relational.isGreaterOrEqualThan
 import ch.ergon.dope.helper.ManagerDependentTest
 import ch.ergon.dope.helper.someBucket
+import ch.ergon.dope.resolvable.expression.unaliased.type.conditional.case
+import ch.ergon.dope.resolvable.expression.unaliased.type.conditional.condition
+import ch.ergon.dope.resolvable.expression.unaliased.type.function.conditional.resultsIn
+import ch.ergon.dope.resolvable.expression.unaliased.type.getString
+import ch.ergon.dope.toDopeType
 import com.schwarz.crystalapi.schema.CMJsonField
 import com.schwarz.crystalapi.schema.CMObjectField
 import com.schwarz.crystalapi.schema.CMObjectList
@@ -72,6 +77,39 @@ class ObjectTest : ManagerDependentTest {
             .selectAsterisk()
             .from(bucket)
             .where(schema.primaryHobby.getField(Hobby::preferredDestination).getField(Destination::groupSize).isGreaterOrEqualThan(5))
+            .build().queryString
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `should support selecting object from list with string`() {
+        val bucket = someBucket().alias("p")
+        val schema = Person("p")
+        val expected = "SELECT * FROM `someBucket` AS `p` WHERE `p`.`hobbies`[0].`name` = `p`.`primaryHobby`.`name`"
+
+        val actual = QueryBuilder()
+            .selectAsterisk()
+            .from(bucket)
+            .where(schema.hobbies.get(0).getString("name").isEqualTo(schema.primaryHobby.getField(Hobby::name)))
+            .build().queryString
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `should support selecting object Field`() {
+        val bucket = someBucket().alias("p")
+        val schema = Person("p")
+        val objectField = schema.primaryHobby.toDopeType()
+        val expected = "SELECT `p`.`primaryHobby`, CASE `p`.`primaryHobby` WHEN `p`.`hobbies`[0] THEN \"isPrimary\" END " +
+            "FROM `someBucket` AS `p` " +
+            "WHERE `p`.`primaryHobby`.`name` = \"Football\""
+
+        val actual = QueryBuilder()
+            .select(objectField, case(objectField).condition(schema.hobbies.get(0).resultsIn("isPrimary")))
+            .from(bucket)
+            .where(objectField.getField(Hobby::name).isEqualTo("Football"))
             .build().queryString
 
         assertEquals(expected, actual)
