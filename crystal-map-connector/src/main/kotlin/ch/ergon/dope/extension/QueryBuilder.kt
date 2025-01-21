d71ef63b14c2de6b5167c655fd744b33c5f26399
Jan Sigrist <jan.sigrist@ergon.ch>
2025-01-21 14:45:43 +0100

    DOPE-277: add select extensions and tests

--- crystal-map-connector/src/main/kotlin/ch/ergon/dope/extension/QueryBuilder.kt
+++ crystal-map-connector/src/main/kotlin/ch/ergon/dope/extension/QueryBuilder.kt
@@ -1,6 +1,7 @@
 package ch.ergon.dope.extension
 
 import ch.ergon.dope.QueryBuilder
+import ch.ergon.dope.resolvable.expression.Expression
 import ch.ergon.dope.toDopeType
 import com.schwarz.crystalapi.schema.CMJsonField
 import com.schwarz.crystalapi.schema.CMJsonList
@@ -9,8 +10,20 @@ import com.schwarz.crystalapi.schema.CMType
 fun QueryBuilder.select(expression: CMType, vararg expressions: CMType) =
     select(expression.toDopeType(), *expressions.map { it.toDopeType() }.toTypedArray())
 
+fun QueryBuilder.select(firstExpression: Expression, secondExpression: CMType, vararg expressions: CMType) =
+    select(firstExpression, *listOf(secondExpression, *expressions).map { it.toDopeType() }.toTypedArray())
+
+fun QueryBuilder.select(firstExpression: CMType, secondExpression: Expression, vararg expressions: Expression) =
+    select(firstExpression.toDopeType(), secondExpression, *expressions)
+
 fun QueryBuilder.selectDistinct(expression: CMType, vararg expressions: CMType) =
     selectDistinct(expression.toDopeType(), *expressions.map { it.toDopeType() }.toTypedArray())
+
+fun QueryBuilder.selectDistinct(firstExpression: Expression, secondExpression: CMType, vararg expressions: CMType) =
+    selectDistinct(firstExpression, *listOf(secondExpression, *expressions).map { it.toDopeType() }.toTypedArray())
+
+fun QueryBuilder.selectDistinct(firstExpression: CMType, secondExpression: Expression, vararg expressions: Expression) =
+    selectDistinct(firstExpression.toDopeType(), secondExpression, *expressions)
 
 @JvmName("selectRawNumber")
 fun QueryBuilder.selectRaw(expression: CMJsonField<Number>) = selectRaw(expression.toDopeType())
